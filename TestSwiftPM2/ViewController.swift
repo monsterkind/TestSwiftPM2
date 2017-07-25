1c10a951bbb7b03d24e3895e097868d8bbfbe424
Andre <137@tranthinktech.com>
2017-07-25 14:48:44 +0800

    Initial Commit

---
+++ TestSwiftPM2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestSwiftPM2
+//
+//  Created by Andre on 25/7/2017.
+//  Copyright © 2017 TranThink. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
